7a5b44cc440327d025e3ec9bc1d5d59436ccbb81
fannndi <72479134+fannndi@users.noreply.github.com>
2025-12-06 23:51:45 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Farewell kernel by Farewell
+kernel.string=Farewell Kernel | MIUI - POCO X3/NFC | by Fannndi
 do.devicecheck=1
 do.modules=0
 do.systemless=1
