e394069a2036e7546a2ff3fbe5df47709a7ad236
fannndi <72479134+fannndi@users.noreply.github.com>
2025-12-06 21:29:18 +0700

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FSociety kernel by Richard
+kernel.string=Farewell kernel by Farewell
 do.devicecheck=1
 do.modules=0
 do.systemless=1
